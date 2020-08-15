52e37274679cb90d08708a527e89796c6b8584e2
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2020-08-15 19:11:54 +0100

    Increase difficulty as it progresses

--- flappy-bird/FlappyBird.gd
+++ flappy-bird/FlappyBird.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var obstacle_timer
 var goal_timer
+var difficulty_timer
+
 var speed = 100
 var branch1
 var goal
@@ -25,13 +27,19 @@ func start():
 	goal_timer = Timer.new()
 	add_child(goal_timer)
 	goal_timer.connect("timeout", self, "prepare_goal")
-	goal_timer.set_wait_time(20.0)
+	goal_timer.set_wait_time(40.0)
 	goal_timer.set_one_shot(false) # Make sure it loops
 	goal_timer.start()
 
-func set_difficulty(difficulty):
-	obstacle_timer.set_wait_time(3 - (0.2 * difficulty))
-	speed = 100 + (20 * difficulty)
+	difficulty_timer = Timer.new()
+	add_child(difficulty_timer)
+	difficulty_timer.connect("timeout", self, "increase_difficulty")
+	difficulty_timer.set_wait_time(5.0)
+	difficulty_timer.set_one_shot(false) # Make sure it loops
+	difficulty_timer.start()
+
+func increase_difficulty():
+	speed = speed + 20
 
 func create_obstacle():
 	var obstacle = branch1.instance()
@@ -39,14 +47,17 @@ func create_obstacle():
 	obstacle.set_speed(speed)
 
 func prepare_goal():
+	print("prepare goal")
 	obstacle_timer.stop()
 	goal_timer.stop()
-	goal_timer.connect("timeout", self, "create_goal")
-	goal_timer.set_wait_time(3.0)
-	goal_timer.start()
+	var new_timer = Timer.new()
+	add_child(new_timer)
+	new_timer.connect("timeout", self, "create_goal")
+	new_timer.set_wait_time(3.0)
+	new_timer.set_one_shot(true) # Make sure it doesn't loop
+	new_timer.start()
 
 func create_goal():
-	goal_timer.stop()
 	var goal_instance = goal.instance()
 	add_child(goal_instance)
 	goal_instance.set_speed(speed)
