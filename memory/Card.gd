b2c158f2da7f91bafc26e900ddb0515117ec8ae9
Joanna Cheng <joanna.cheng@softwire.com>
2020-08-15 13:29:15 +0100

    Memory game initial

---
+++ memory/Card.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+var allow_click
+var front_image
+var back_image
+var card_name
+
+func _ready():
+	card_name = "Not initialized"
+	allow_click = true
+	front_image = preload("res://assets/memory/card-back.png")
+	back_image = preload("res://assets/memory/card-back.png")
+	get_node("Sprite").texture = back_image
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton \
+	and event.button_index == BUTTON_LEFT \
+	and event.is_pressed():
+		self.on_click()
+		
+func on_click():
+	if allow_click:
+		allow_click = false
+		get_node("Sprite").texture = front_image
+		if (get_parent().selected_card1_image == "Card 1"):
+			get_parent().selected_card1_image = card_name
+			get_parent().selected_card1 = name
+		elif (get_parent().selected_card2_image == "Card 2"):
+			get_parent().selected_card2_image = card_name
+			get_parent().selected_card2 = name
+			get_parent().check_if_match()
---
+++ memory/MemoryGame.gd
@@ -0,0 +1,113 @@
+extends Node2D
+
+var images
+var card_default
+var number_of_cards
+var selected_card1
+var selected_card2
+var selected_card1_image
+var selected_card2_image
+var number_of_matches
+
+func _ready():
+	hide()
+	
+func start():
+	number_of_matches = 0
+	selected_card1_image = "Card 1"
+	selected_card2_image = "Card 2"
+	card_default = preload("res://assets/memory/card-back.png")
+	images = [
+		{
+			"image": preload("res://assets/memory/leaf-1-card.png"),
+			"name": "Green Leaf"
+		},
+		{
+			"image": preload("res://assets/memory/leaf-2-card.png"),
+			"name": "Red Leaf"
+		},
+		{
+			"image": preload("res://assets/memory/sun-card.png"),
+			"name": "Sun"
+		},
+		{
+			"image": preload("res://assets/memory/bird-feeder-card.png"),
+			"name": "Bird Feeder"
+		},
+		{
+			"image": preload("res://assets/memory/twig-1-card.png"),
+			"name": "Branch"
+		},
+		{
+			"image": preload("res://assets/memory/twig-2-card.png"),
+			"name": "Twig"
+		},
+		{
+			"image": preload("res://assets/memory/water-card.png"),
+			"name": "Water"
+		},
+		{
+			"image": preload("res://assets/memory/worm-card.png"),
+			"name": "Worm"
+		}
+	]
+	number_of_cards = images.size() * 2
+	shuffle_cards()
+	
+func shuffle_cards():
+	var card_numbers = range(number_of_cards)
+	card_numbers.shuffle()
+	
+	for i in range(number_of_cards / 2):
+		var pair_card1 = "Card" + String(card_numbers[2*i])
+		var pair_card2 = "Card" + String(card_numbers[2*i+1])
+		get_node(pair_card1).front_image = images[i].image
+		get_node(pair_card1).card_name = images[i].name
+		get_node(pair_card2).front_image = images[i].image
+		get_node(pair_card2).card_name = images[i].name
+	
+		
+func check_if_match():
+	disable_card_clicks()
+	var matched = selected_card1_image == selected_card2_image
+	if matched:
+		number_of_matches += 1
+		get_node("NumberMatchesFound").text = String(number_of_matches)
+		
+#	if number_of_matches == number_of_cards / 2:
+#		get_tree().reload_current_scene()
+	var waiting_timer = Timer.new()
+	waiting_timer.set_wait_time(1)
+	waiting_timer.set_one_shot(true)
+	self.add_child(waiting_timer)
+	waiting_timer.start()
+	
+	yield(waiting_timer, "timeout")
+	reset_selected_card_vars()
+	handle_selected_cards(matched)
+	enable_card_clicks()
+		
+func disable_card_clicks():
+	for i in range(number_of_cards):
+		var card = "Card" + String(i)
+		get_node(card).allow_click = false
+		
+func reset_selected_card_vars():
+	selected_card1_image = "Card 1"
+	selected_card2_image = "Card 2"
+	
+func handle_selected_cards(matched):
+	if (matched):
+		get_node(selected_card1).visible = false
+		get_node(selected_card2).visible = false
+	else:
+		get_node(selected_card1).get_node("Sprite").texture = card_default
+		get_node(selected_card2).get_node("Sprite").texture = card_default
+	
+func enable_card_clicks():
+	for i in range(number_of_cards):
+		var card = "Card" + String(i)
+		if (get_node(card).get_node("Sprite").texture == card_default):
+			get_node(card).allow_click = true
+
+
