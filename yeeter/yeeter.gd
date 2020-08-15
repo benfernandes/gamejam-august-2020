1b4cb03dbeb51fb4132164aa3fe0356b5cc937e6
Ben Fernandes <benjamin.fernandes@softwire.com>
2020-08-15 23:24:02 +0100

    WIP Fixed egg counting system

--- yeeter/yeeter.gd
+++ yeeter/yeeter.gd
@@ -1,6 +1,9 @@
 extends Control
 
+signal eggs_stopped_moving
+
 var is_playing = false
+var eggs_moving = false
 
 # Remaining bar
 export (NodePath) var remaining_path
@@ -18,6 +21,16 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_up"):
 		print("Number of eggs")
 		print(get_tree().get_nodes_in_group("eggs").size())
+		
+	eggs_moving = _are_eggs_moving()
+	print(eggs_moving)
+
+func _are_eggs_moving():
+	var eggs = get_tree().get_nodes_in_group("eggs")
+	for egg in eggs:
+		if !egg.sleeping:
+			return true
+	return false
 
 # Transition from start screen to game
 func _on_start_overlay_ready_to_start():
@@ -26,8 +39,7 @@ func _on_start_overlay_ready_to_start():
 # Transition from game to end screen
 func _on_remaining_none_remaining():
 	is_playing = false
-	$end_overlay.show()
-
+	
 # Transition from end screen to home screen
 func _on_end_overlay_ready_to_end():
 	close_game()
@@ -38,3 +50,7 @@ func _on_back_button_pressed():
 func close_game():
 	get_parent().show_main_screen()
 	queue_free()
+
+func _on_yeeter_eggs_stopped_moving():
+	if !is_playing:
+		$end_overlay.show()
