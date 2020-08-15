078dd9c87f85f76334a909770ec8af730665ce9d
Ben Fernandes <benjamin.fernandes@softwire.com>
2020-08-15 16:34:14 +0100

    Merge fixes

--- Main.gd
+++ Main.gd
@@ -2,24 +2,35 @@ extends Node
 
 var memory_game
 var memory_game_instance
+var egg_yeeter_game
+var egg_yeeter_game_instance
 
 func _ready():
 	memory_game = preload("res://memory/MemoryGameMain.tscn")
 	memory_game_instance = memory_game.instance()
-	# instantiate other minigames
 	
+	egg_yeeter_game = preload("res://minigames/yeeter/yeeter.tscn")
+	egg_yeeter_game_instance = egg_yeeter_game.instance()
+	# instantiate other minigames
+
+func _hide_main_screen():
+	$MemoryGameButton.hide()
+	$EggYeeterButton.hide()
+	# add the other buttons to hide here
+
+func _show_main_screen():
+	$MemoryGameButton.show()
+	$EggYeeterButton.show()
+	memory_game_instance = memory_game.instance()
+	egg_yeeter_game_instance = egg_yeeter_game.instance()
+	# add the other buttons to show here and reinstantiate the game
+
 func _on_MemoryGameButton_pressed():
 	_hide_main_screen()
 	add_child(memory_game_instance)
-	
+
+func _on_EggYeeterButton_pressed():
+	_hide_main_screen()
+	add_child(egg_yeeter_game_instance)
+
 # add functions to play other minigames here
-	
-func _hide_main_screen():
-	$MemoryGameButton.hide()
-	# add the other buttons to hide here
-	
-func _show_main_screen():
-	$MemoryGameButton.show()
-	memory_game_instance = memory_game.instance()
-	# add the other buttons to show here and reinstantiate the game
-	
