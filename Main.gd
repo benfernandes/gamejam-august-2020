6a4cd65be5a305e9bfbf9e2a0458bd0fc3ede79e
Ben Fernandes <benjamin.fernandes@softwire.com>
2020-08-16 12:34:44 +0100

    Changed where we create game instances in the Main scene

--- Main.gd
+++ Main.gd
@@ -6,17 +6,13 @@ var egg_yeeter_game
 var egg_yeeter_game_instance
 var flappy_bird
 var flappy_bird_instance
+var difficulty = "easy"
 
 func _ready():
 	memory_game = preload("res://memory/MemoryGameMain.tscn")
-	memory_game_instance = memory_game.instance()
-
 	flappy_bird = preload("res://flappy-bird/FlappyBirdMain.tscn")
-	flappy_bird_instance = flappy_bird.instance()
-
 	egg_yeeter_game = preload("res://yeeter/yeeter.tscn")
-	egg_yeeter_game_instance = egg_yeeter_game.instance()
-	# instantiate other minigames
+	# preload other minigames
 
 func _hide_main_screen():
 	$MemoryGameButton.hide()
@@ -28,20 +24,20 @@ func show_main_screen():
 	$MemoryGameButton.show()
 	$EggYeeterButton.show()
 	$FlappyBirdButton.show()
-	memory_game_instance = memory_game.instance()
-	egg_yeeter_game_instance = egg_yeeter_game.instance()
-	flappy_bird_instance = flappy_bird.instance()
-	# add the other buttons to show here and reinstantiate the game
+	# add the other buttons to show here
 
 func _on_MemoryGameButton_pressed():
+	memory_game_instance = memory_game.instance()
 	_hide_main_screen()
 	add_child(memory_game_instance)
 
 func _on_EggYeeterButton_pressed():
+	egg_yeeter_game_instance = egg_yeeter_game.instance()
 	_hide_main_screen()
 	add_child(egg_yeeter_game_instance)
 	
 func _on_FlappyBirdButton_pressed():
+	flappy_bird_instance = flappy_bird.instance()
 	_hide_main_screen()
 	add_child(flappy_bird_instance)
 
