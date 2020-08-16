1466f88134a958365912bee43199461b09efb621
Ben Fernandes <benjamin.fernandes@softwire.com>
2020-08-16 19:25:37 +0100

    Only using one text box for game start screens

--- Main.gd
+++ Main.gd
@@ -8,6 +8,10 @@ var scene_instance
 var current_difficulty = "easy"
 var current_act = 1
 
+var flappy_text = "It's time to fly the nest and find somewhere to settle down.\n\nTap space to fly and avoid the obstacles!"
+var memory_text = "This place looks nice. Better build a nest!\n\nClick a card to flip it and reveal something you might want for your nest.\n\nFind all the matching pairs within the time limit to win the game!"
+var yeeter_text = "What a lovely nest! Well done. Now time to lay some eggs!\n\nHold space to lay an egg and use the left and right buttons to aim. Release the spacebar to  shoot the egg out.\n\nGet as many eggs as you can into the backet!"
+
 func _ready():
 	hide_main_screen()
 	flappy_bird = preload("res://flappy-bird/FlappyBirdMain.tscn")
@@ -16,11 +20,9 @@ func _ready():
 	hatch_scene = preload("res://hatching/Hatching.tscn")
 
 func hide_main_screen():
-	$MemoryGameTextBox.hide()
+	$MainTextBox.hide()
 	$MemoryGameButton.hide()
-	$EggYeeterTextBox.hide()
 	$EggYeeterButton.hide()
-	$FlappyBirdTextBox.hide()
 	$FlappyBirdButton.hide()
 
 func _on_main_start_game():
@@ -36,15 +38,18 @@ func start_scene(text_node, button_node, scene, scene_name):
 
 # Running Flappy Bird scene
 func start_flappy_scene():
-	start_scene($FlappyBirdTextBox, $FlappyBirdButton, flappy_bird, "flappy")
+	$MainTextBox/MainText.text = flappy_text
+	start_scene($MainTextBox, $FlappyBirdButton, flappy_bird, "flappy")
 
 # Running Memory scene
 func start_memory_scene():
-	start_scene($MemoryGameTextBox, $MemoryGameButton, memory_game, "memory")
+	$MainTextBox/MainText.text = memory_text
+	start_scene($MainTextBox, $MemoryGameButton, memory_game, "memory")
 
 # Running Yeeter scene
 func start_yeeter_scene():
-	start_scene($EggYeeterTextBox, $EggYeeterButton, egg_yeeter_game, "yeeter")
+	$MainTextBox/MainText.text = yeeter_text
+	start_scene($MainTextBox, $EggYeeterButton, egg_yeeter_game, "yeeter")
 
 # Handling start scene button pressed
 func _on_start_scene_button_pressed():
